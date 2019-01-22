b44a33353531e31c66aa13aa2af7e2c0b98e0a16
Jae-eun <je4297@naver.com>
2019-01-22 16:55:40 +0900

    make ViewController and request HealthKit Capabilites

--- healthKitPractice/ViewController.swift
+++ healthKitPractice/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import HealthKit
 
 class ViewController: UIViewController {
 
+  let healthStore = HKHealthStore()
+  
+  let stepType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+  let woType = HKObjectType.workoutType()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    healthStore.requestAuthorization(toShare: [],
+                               read: [stepType, woType],
+                               completion: { (isSuccess, error) in
+      if isSuccess {
+        print("Success")
+        self.getSteps()
+      } else {
+        print("Failed")
+      }
+    })
   }
 
-
+  func getSteps() {
+    let startDate = Calendar.current.date(bySettingHour: 0, minute: 0, second: 0, of: Date())!
+    
+    let endDate = Date()
+    
+    print("Collecting workouts between \(startDate) and \(endDate)")
+    
+    let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: HKQueryOptions.strictEndDate)
+    
+    let query = HKSampleQuery(sampleType: HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning)!, predicate: predicate, limit: HKObjectQueryNoLimit, sortDescriptors: nil) { (query, results, error) in
+      if let results = results {
+        for item in results {
+          print(item)
+        }
+      }
+    }
+    healthStore.execute(query)
+  }
 }
 
