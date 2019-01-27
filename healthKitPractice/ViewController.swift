bc5c07419693357bb1bb1d83c7d2de8844b79a40
Jae-eun <je4297@naver.com>
2019-01-27 23:33:04 +0900

    HealthKitManager의 findHealthKitValue 구현

--- healthKitPractice/ViewController.swift
+++ healthKitPractice/ViewController.swift
@@ -11,73 +11,95 @@ import HealthKit
 
 class ViewController: UIViewController {
   
+  fileprivate var healthKitManager = HealthKitManager()
+  
   @IBOutlet weak var stepLabel: UILabel!
   @IBOutlet weak var distanceLabel: UILabel!
   
-  let healthStore = HKHealthStore()
   
-  let stepType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
-  let distanceType = HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning)!
+  let endDate = Date()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    healthStore.requestAuthorization(toShare: [],
-                                     read: [stepType, distanceType],
-                                     completion: { (isSuccess, error) in
-                                      if isSuccess {
-                                        print("Success")
-                                        self.getSteps()
-                                      } else {
-                                        print("Failed")
-                                      }
-    })
-  }
-  
-  func getSteps() {
     guard let startDate = Calendar.current.date(bySettingHour: 0, minute: 0, second: 0, of: Date()) else { return }
     
-    let endDate = Date()
+    healthKitManager.retrieveHealthKitValue(startDate: startDate, endDate: endDate, quantityFor: HKUnit.meter(), quantityTypeIdentifier: .distanceWalkingRunning) { distance in
+        DispatchQueue.main.async {
+          self.distanceLabel.text = "\(distance) km"
+        }
+    }
     
-    print("Collecting workouts between \(startDate) and \(endDate)")
-    
-    let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: HKQueryOptions.strictEndDate)
- 
-    let query = HKStatisticsQuery(quantityType: stepType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (_, result, error) in
-      var resultCount = 0
-      guard let result = result else {
-        print("Failed to fetch steps rate")
-        return
-      }
-      if let sum = result.sumQuantity() {
-        resultCount = Int(sum.doubleValue(for: HKUnit.count()))
-      }
+    healthKitManager.retrieveHealthKitValue(startDate: startDate, endDate: endDate, quantityFor: HKUnit.count(), quantityTypeIdentifier: .stepCount) { stepCount in
       DispatchQueue.main.async {
-        self.stepLabel.text = "\(resultCount) 걸음"
+        self.stepLabel.text = "\(stepCount) 걸음"
       }
     }
-    let query2 = HKStatisticsQuery(quantityType: distanceType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (_, result2, error) in
-      var resultCount2 = 0.0
-      guard let result2 = result2 else {
-        print("Failed to fetch distances")
-        return
-      }
-      if let sum2 = result2.sumQuantity() {
-        resultCount2 = sum2.doubleValue(for: HKUnit.meter())
-      }
-      var distance = ""
-      
-      if resultCount2 < 1000 {
-        distance = "\(resultCount2) + m"
-      } else {
-        let distanceInKilometer = resultCount2 / 1000
-        distance = String(format:"%.1f", distanceInKilometer) + " km"
-      }
-      DispatchQueue.main.async {
-        self.distanceLabel.text = "\(distance)"
-      }
-    }
-    healthStore.execute(query)
-    healthStore.execute(query2)
   }
+  //  let healthStore = HKHealthStore()
+  //
+  //  let stepType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+  //  let distanceType = HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning)!
+  //
+  //  override func viewDidLoad() {
+  //    super.viewDidLoad()
+  //
+  //    healthStore.requestAuthorization(toShare: [],
+  //                                     read: [stepType, distanceType],
+  //                                     completion: { (isSuccess, error) in
+  //                                      if isSuccess {
+  //                                        print("Success")
+  //                                        self.getSteps()
+  //                                      } else {
+  //                                        print("Failed")
+  //                                      }
+  //    })
+  //  }
+  //
+  //  func getSteps() {
+  //    guard let startDate = Calendar.current.date(bySettingHour: 0, minute: 0, second: 0, of: Date()) else { return }
+  //
+  //    let endDate = Date()
+  //
+  //    print("Collecting workouts between \(startDate) and \(endDate)")
+  //
+  //    let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: HKQueryOptions.strictEndDate)
+  //
+  //    let query = HKStatisticsQuery(quantityType: stepType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (_, result, error) in
+  //      var resultCount = 0
+  //      guard let result = result else {
+  //        print("Failed to fetch steps rate")
+  //        return
+  //      }
+  //      if let sum = result.sumQuantity() {
+  //        resultCount = Int(sum.doubleValue(for: HKUnit.count()))
+  //      }
+  //      DispatchQueue.main.async {
+  //        self.stepLabel.text = "\(resultCount) 걸음"
+  //      }
+  //    }
+  //    let query2 = HKStatisticsQuery(quantityType: distanceType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (_, result2, error) in
+  //      var resultCount2 = 0.0
+  //      guard let result2 = result2 else {
+  //        print("Failed to fetch distances")
+  //        return
+  //      }
+  //      if let sum2 = result2.sumQuantity() {
+  //        resultCount2 = sum2.doubleValue(for: HKUnit.meter())
+  //      }
+  //      var distance = ""
+  //
+  //      if resultCount2 < 1000 {
+  //        distance = "\(resultCount2) + m"
+  //      } else {
+  //        let distanceInKilometer = resultCount2 / 1000
+  //        distance = String(format:"%.1f", distanceInKilometer) + " km"
+  //      }
+  //      DispatchQueue.main.async {
+  //        self.distanceLabel.text = "\(distance)"
+  //      }
+  //    }
+  //    healthStore.execute(query)
+  //    healthStore.execute(query2)
+  //  }
 }
